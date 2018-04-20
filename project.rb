3f8eb760aceb0ad861ecb9267490084afb2781d0
Chris Rithmyxay <crithmyxay@gmail.com>
2018-04-20 11:58:29 -0400

    fixes error on line 9

--- project.rb
+++ project.rb
@@ -6,9 +6,9 @@ result = []
 #opens new file with second argument and copies first argument over moving class to beginning and removing "Triceps skin fold thickness (mm)" column
 CSV.open(ARGV[1], "w+") do |csv|
   table.each do |line|
-    result << [line[9], line[0], line[1], line[2], line[3], line[5], line[6], line[7], line[8]]
+    csv << [line[9], line[0], line[1], line[2], line[3], line[5], line[6], line[7], line[8]]
   end
 end
 
-result.each {|row| puts row}
+
 
