b5b02e8f1dd74a5e4bf982b63d4e3c66926bf062
Chris Rithmyxay <crithmyxay@gmail.com>
2018-04-24 08:09:29 -0400

    meets all requirements

--- project.rb
+++ project.rb
@@ -2,37 +2,22 @@
 require 'csv'
 
 table = CSV.read(ARGV[0])
-result = Array.new
-new_result = []
-
-#opens new file with second argument and copies first argument over moving class to beginning and removing "Triceps skin fold thickness (mm)" column
-# CSV.open(ARGV[1], "w+") do |csv|
+=begin
+opens new file with second argument and copies first argument over 
+moving class to beginning and removing "Triceps skin fold thickness (mm)" column
+adds column and takes age in years and converts to age in days
+=end
+CSV.open(ARGV[1], "w+") do |csv|
+  i = 0
   table.each do |line|
-    result << [line[9], line[0], line[1], line[2], line[3], line[5], line[6], line[7], line[8]]
-  end
-# end
-
-i = 0
-result.each do |row|
-  if i == 0
-    new_result << "Age(days)"
-    i += 1
-  else
-    cell = row[row.length-1].to_i*365
-    new_result << cell
-    i += 1
+    new_line = ''
+    if i == 0 
+      new_line = "Age(days)"
+      i += 1
+    else
+      new_line = line[8].to_i*365
+      # i += 1
+    end
+    csv << [line[9], line[0], line[1], line[2], line[3], line[5], line[6], line[7], line[8], new_line]
   end
 end
-
-CSV.open(ARGV[1], 'w') do |csv|
-  j=0
-  new_result.each do |row|
-    result[j] << row
-    j += 1
-  end
-  csv << result
-end
-
-
-# result.each { |row| puts row.inspect}
-# print result
